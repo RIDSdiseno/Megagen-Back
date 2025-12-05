671b19e523c54319528e7e063117d38828cee1c4
RIDSdiseno <diseno@rids.cl>
2025-12-05 16:37:40 -0300

    remove node_modules

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,28 @@ import { notFound } from "./middleware/notFound";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://megagen.netlify.app"
+];
+// Necesario para Railway / proxies HTTPS
+app.set("trust proxy", 1);
 
-app.use(cors());
+// CORS configurado para aceptar peticiones desde tu frontend en Netlify
+app.use(
+  cors({
+    origin: allowedOrigins,  
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
+// Rutas principales
 app.use("/api", routes);
 
+// Middlewares globales
 app.use(notFound);
 app.use(errorHandler);
 
